7b5751b3bed7d24d41f76fa1dff04d4cf17d0573
Pratham <ssahasrabhojane@gmail.com>
2020-08-17 22:05:20 +0530

    GraphQL test

--- actions/main.py
+++ actions/main.py
@@ -1,1 +1,12 @@
-print("hii")
+import sys
+import json
+
+parsed_json=json.loads(sys.argv[1])
+
+image_url=str(parsed_json['user']['avatarUrl'])
+email=str(parsed_json['user']['email'])
+username=str(parsed_json['user']['login'])
+print(sys.argv[2])
+print(image_url)
+print(email)
+print(username)
